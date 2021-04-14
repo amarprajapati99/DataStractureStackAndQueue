4cbb89bbd8c272fb5a21676b3ffc3c5bbb2c101e
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-14 10:34:12 +0530

    To create a Queue of 56->30->70

---
+++ src/main/java/com/stackandqueue/MyQueue.java
@@ -0,0 +1,23 @@
+package com.stackandqueue;
+
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue(){
+        this.myLinkedList = new MyLinkedList();
+    }
+
+
+    public void enqueue(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue(){
+        myLinkedList.printMyNodes();
+    }
+
+    public INode peak(){
+        return myLinkedList.head;
+    }
+
+}
---
+++ src/test/java/com/stackandqueue/MyQueueTest.java
@@ -0,0 +1,20 @@
+package com.stackandqueue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+/* To create a Queue of 56->30->70 */
+public class MyQueueTest {
+    @Test
+    public void given3IntegersWhenAddedToQueueShouldPassTheTest(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        INode peak = myQueue.peak();
+        myQueue.printQueue();
+        Assertions.assertEquals(peak, myFirstNode);
+    }
+}
