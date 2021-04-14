9eba1d6b5041c511a4dcd66397b8a67eff7231ad
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-14 10:24:32 +0530

    To peak and pop from the stack

--- src/test/java/com/stackandqueue/MyStackTest.java
+++ src/test/java/com/stackandqueue/MyStackTest.java
@@ -2,10 +2,11 @@ package com.stackandqueue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-/* @Description- To create a Stack of 56->30->70 */
+/* @Description- To create a Stack of 56->30->70
+* To peak and pop from the Stack till it is empty 56->30->70 */
 public class MyStackTest {
     @Test
-    public void given3Numbers_WhenAddedToStack_ShouldHaveLastAddedNode() {
+    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
         MyStack myStack = new MyStack();
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -16,6 +17,20 @@ public class MyStackTest {
         INode peak = myStack.peak();
         myStack.printStack();
         Assertions.assertEquals(myThirdNode, peak);
+    }
+/* @Description-To peak and pop from the Stack till it is empty 56->30->70 */
+    @Test
+    public void given3NumbersInStackWhenPopedShouldMatchWithLastAddedNode() {
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode pop = myStack.pop();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode, pop);
     }
 }
 
