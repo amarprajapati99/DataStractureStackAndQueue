bb35c36df89c36955cc4f9291122a9972db2b06f
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-14 10:11:20 +0530

    To create a Stack of 56->30->70

--- src/test/java/com/stackandqueue/MyStackTest.java
+++ src/test/java/com/stackandqueue/MyStackTest.java
@@ -1,4 +1,21 @@
 package com.stackandqueue;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+/* @Description- To create a Stack of 56->30->70 */
 public class MyStackTest {
+    @Test
+    public void given3Numbers_WhenAddedToStack_ShouldHaveLastAddedNode() {
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode peak = myStack.peak();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode, peak);
+    }
 }
+
